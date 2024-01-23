1087d49e360034146e9fe53424d409436c0ff47e
Alán F. Muñoz <amunozgo@broadinstitute.org>
2024-01-23 15:10:22 -0500

    fix(process): rm timing info

--- src/process.py
+++ src/process.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env jupyter
-from time import perf_counter
 
 import numpy as np
 import pandas as pd
 from copairs.map import aggregate, run_pipeline
 from pathos.multiprocessing import Pool
 from pycytominer.cyto_utils import load_profiles
-
 from utils import get_featuredata, get_metadata
 
 
@@ -21,7 +19,6 @@ def split_and_process(data: pd.DataFrame, **kwargs):
 
 def run_pipeline_and_aggregate(data, pipeline_type, **kwargs) -> pd.DataFrame:
     data = data.loc[data["Metadata_Pipeline"] == pipeline_type]
-    print(f"Pipeline run in {perf_counter()-t} seconds")
 
     result = split_and_process(data)
     aggregated = aggregate(
@@ -40,9 +37,7 @@ def calculate_map_from_filename(filename, **kwargs):
     #     data[i]["Metadata_Pipeline"] = filename.name.split(".")[0]
 
     print("Loading data")
-    t = perf_counter()
     data = load_profiles(filename)
-    print(f"Data loaded in {perf_counter()-t} seconds")
     data["Metadata_Pipeline"] = filename.name.split(".")[0]
 
     data["Metadata_negcon"] = np.where(data["Metadata_pert_type"] == "negcon", 1, 0)
