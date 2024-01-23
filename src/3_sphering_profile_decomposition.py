70763bef5640f590d957aba36e88f2cc7edc9d24
Alán F. Muñoz <amunozgo@broadinstitute.org>
2024-01-23 15:09:54 -0500

    deps(sphering): rm black,isort; add polars

--- src/3_sphering_profile_decomposition.py
+++ src/3_sphering_profile_decomposition.py
@@ -13,24 +13,27 @@
 #     name: python3
 # ---
 
-# %% [markdown]
+# %% [markdown] Goal/Summary
 """
 Goal: Find out the reason behind the spikes caused by global and batch sphering
 
+Conclusions:
+ - Spikes are not produced during data aggregation, they arise during sphering
 """
-# %%
+# %% Imports
 
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import seaborn as sns
 
 from process import split_and_process
 
+# %% Setup and data load
 run_file = "20231111_175146/sphering_Metadata_Batch.parquet"
 profile_dir = Path(f"./runs/{run_file}")
+figs_dir = Path("../figs")
 
 kwargs = dict(
     pos_sameby=["Metadata_JCP2022"],
@@ -41,46 +44,27 @@ kwargs = dict(
     batch_size=20000,
 )
 
-# %%
+# %% Load data
 profiles = pd.read_parquet(profile_dir)
 profiles["Metadata_negcon"] = np.where(profiles["Metadata_pert_type"] == "negcon", 1, 0)
-
+# %% Run AP pipeline and aggregate
 result = split_and_process(profiles, **kwargs)
-tmp = (
-    result.groupby(["Metadata_Batch", "Metadata_JCP2022"])["average_precision"]
-    .mean()
-    .reset_index()
-)
-
-sns.stripplot(
-    data=tmp,
-    y="Metadata_Batch",
-    x="average_precision",
-    dodge=True,
-    hue="Metadata_JCP2022",
-)
-plt.savefig(figs_dir / "AP_perBatch.png", dpi=250)
-plt.close()
-
-# Check the correlation between aggregated mAP and number of batches in which they occur
-
 aggregation = result.groupby(pos_sameby)["average_precision"].agg(["mean", "count"])
+# %% Prepare plot aestheti
+# %% Plot ap vs count
 sns.scatterplot(data=aggregation, x="mean", y="count")
 plt.savefig(figs_dir / "scatter_map_mean_count.png")
 plt.close()
 
-result_sorted = (
-    result["average_precision"]
-    .value_counts()
-    .reset_index()
-    .sort_values("average_precision")
-)
 
-# %% Check the composition of the spikes
+#     result["average_precision"]
+#     .value_counts()
+#     .reset_index()
+#     .sort_values("average_precision")
+# )
 
-mapper = {
-    k: f"Batch_{ k[16:].rjust(2,'0') }" for k in result["Metadata_Batch"].unique()
-}
+# %% Check which batches compose the spikes
+
 ax = sns.histplot(
     data=result.replace(mapper).sort_values("Metadata_Batch"),
     x="average_precision",
@@ -95,41 +79,27 @@ plt.tight_layout()
 plt.savefig(figs_dir / "histplot_preagg_ap_mean_count.png")
 plt.close()
 
-tmp = (
+# %% Group by Batch and JCP
+batch_jcp_grouped = (
     result.groupby(["Metadata_Batch", "Metadata_JCP2022"])["average_precision"]
     .mean()
     .reset_index()
 )
+batch_jcp_grouped = batch_jcp_grouped.replace(mapper).sort_values("Metadata_Batch")
+# %% Violin plot to show batch
 
-sns.stripplot(
-    data=tmp,
+sns.violinplot(
+    data=batch_jcp_grouped,
     y="Metadata_Batch",
     x="average_precision",
-    dodge=True,
-    hue="Metadata_JCP2022",
+    hue="Metadata_Batch",
 )
 plt.savefig(figs_dir / "AP_perBatch.png", dpi=250)
 plt.close()
 
-# Check the correlation between aggregated mAP and number of batches in which they occur
+# %% Ongoing work
 
-aggregation = result.groupby(pos_sameby)["average_precision"].agg(["mean", "count"])
-sns.scatterplot(data=aggregation, x="mean", y="count")
-plt.savefig(figs_dir / "scatter_map_mean_count.png")
-plt.close()
 
-result_sorted = (
-    result["average_precision"]
-    .value_counts()
-    .reset_index()
-    .sort_values("average_precision")
-)
-
-# %% Check which batches compose the spikes
-
-mapper = {
-    k: f"Batch_{ k[16:].rjust(2,'0') }" for k in result["Metadata_Batch"].unique()
-}
 ax = sns.histplot(
     data=result.replace(mapper).sort_values("Metadata_Batch"),
     x="average_precision",
